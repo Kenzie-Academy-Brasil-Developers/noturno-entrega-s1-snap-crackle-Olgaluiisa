6efbc2da34d1c4ce2e4c4237735e966c7ffdabb1
Olgaluiisa <olgaluiisa@hotmail.com>
2021-10-26 02:23:25 -0300

    trab-snapCrackle

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,45 @@
-// Seu código vai aqui
+
+
+function snapCrackle(maxValue){
+    let texto = '';
+    for(let contador = 1; contador < maxValue; contador++){
+           if(contador % 2 !== 0 && contador % 5 === 0){
+            texto+= ' SnapCrackle, ';
+         }else if(contador % 2 !== 0){
+            texto+= ' Snap, ';
+         }else if(contador % 5 === 0){
+             texto+= ' Crackle, ';
+         }else{
+             texto+= contador;
+         }
+
+        
+          
+    }
+        return texto;
+}
+
+console.log(snapCrackle(15))
+
+// numeros primos
+
+function  snapCracklePrime(maxValeu){
+    let texto = '';
+    for(let contador = 1; contador < maxValeu; contador++){
+           if(contador % 2 !== 0 && contador % 5 === 0){
+            texto+= ' SnapCrackle, ';
+         }else if(contador % 2 !== 0){
+            texto+= ' Snap, ';
+         }else if(contador % 5 === 0){
+             texto+= ' Crackle,';
+         }else if(contador % 1 === 0 && contador % contador === 0){
+            texto+= 'Prime, ';
+         }else{
+             texto+= contador;
+         }       
+          
+    }
+        return texto;
+}
+
+console.log( snapCracklePrime(15))
