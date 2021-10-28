5399df089eea524486c79984488dbf3538d11569
Olgaluiisa <olgaluiisa@hotmail.com>
2021-10-28 18:41:05 -0300

    SnapCrackle-refeito

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,17 +1,17 @@
 
-
 function snapCrackle(maxValue){
     let texto = '';
-    for(let contador = 1; contador < maxValue; contador++){
+    for(let contador = 1; contador <= maxValue; contador++){
            if(contador % 2 !== 0 && contador % 5 === 0){
-            texto+= ' SnapCrackle, ';
+            texto+='SnapCrackle,' + ' ';
          }else if(contador % 2 !== 0){
-            texto+= ' Snap, ';
+            texto+='Snap,' + ' ';
          }else if(contador % 5 === 0){
-             texto+= ' Crackle, ';
+             texto+='Crackle,'+ ' ';
          }else{
-             texto+= contador;
+             texto+= contador + ', ';
          }
+
 
         
           
@@ -19,27 +19,7 @@ function snapCrackle(maxValue){
         return texto;
 }
 
-console.log(snapCrackle(15))
+console.log(snapCrackle(15));
 
-// numeros primos
 
-function  snapCracklePrime(maxValeu){
-    let texto = '';
-    for(let contador = 1; contador < maxValeu; contador++){
-           if(contador % 2 !== 0 && contador % 5 === 0){
-            texto+= ' SnapCrackle, ';
-         }else if(contador % 2 !== 0){
-            texto+= ' Snap, ';
-         }else if(contador % 5 === 0){
-             texto+= ' Crackle,';
-         }else if(contador % 1 === 0 && contador % contador === 0){
-            texto+= 'Prime, ';
-         }else{
-             texto+= contador;
-         }       
-          
-    }
-        return texto;
-}
 
-console.log( snapCracklePrime(15))
